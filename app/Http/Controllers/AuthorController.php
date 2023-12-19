60819f5c290e018920c2940bc78fdd8689b5962a
Lluis Alfonso <lluisalfonso10@gmail.com>
2023-12-19 19:41:50 +0100

    [store-author-request] Add author store request & add returning type declaration on AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,40 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAuthorRequest;
 use App\Models\Author;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class AuthorController extends Controller
 {
-    public function index() {
-        $authors = Author::paginate(5);
+    public function index(): View {
+        $authors = Author::paginate(20);
+
         return view('authors.index', ['authors' => $authors]);
     }
 
-    public function create() {
+    public function create(): View {
         return view('authors.create');
     }
 
-    public function store(Request $request) {
-        $data = $request->validate([
-            'name' => 'required',
-            'description' => 'nullable'
-        ]);
+    public function store(StoreAuthorRequest $request): RedirectResponse {
+        $data = $request->validated();
 
         $newAuthor = Author::create($data);
 
         return redirect(route('author.index'));
     }
 
-    public function edit(Author $author) {
+    public function edit(Author $author): View {
         return view('authors.edit', ['author' => $author]);
     }
 
-    public function update(Author $author, Request $request) {
-        $data = $request->validate([
-            'name' => 'required',
-            'description' => 'nullable'
-        ]);
+    public function update(Author $author, StoreAuthorRequest $request): RedirectResponse {
+        $data = $request->validated();
 
         $author->update($data);
 
@@ -43,7 +40,7 @@ class AuthorController extends Controller
     }
 
 
-    public function delete(Author $author) {
+    public function delete(Author $author): RedirectResponse {
         $author->delete();
 
         return redirect(route('author.index'))->with('success', 'Autor eliminado correctamente!');
