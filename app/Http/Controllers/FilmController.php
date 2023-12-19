94aebe92f0faba17e87510daa8720abc307bc0c9
Lluis Alfonso <lluisalfonso10@gmail.com>
2023-12-19 18:28:34 +0100

    [store-film-request] Add film store request

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreFilmRequest;
 use App\Models\Director;
 use App\Models\Film;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
     public function index() {
         $films = Film::paginate(5);
+        
         return view('films.index', ['films' => $films]);
     }
 
@@ -18,14 +21,8 @@ class FilmController extends Controller
         return view('films.create', ['directors' => $directors]);
     }
 
-    public function store(Request $request) {
-        $data = $request->validate([
-            'director_id' => 'nullable',
-            'title' => 'required',
-            'rating' => 'nullable|decimal:0,2|between:0,10',
-            'has_seen' => 'nullable',
-            'description' => 'nullable'
-        ]);
+    public function store(StoreFilmRequest $request): RedirectResponse {
+        $data = $request->validated();
 
         $data['has_seen'] = $this->checkboxToBoolean($data);
 
@@ -37,14 +34,8 @@ class FilmController extends Controller
         return view('films.edit', ['film' => $film]);
     }
 
-    public function update(Film $film, Request $request) {
-        $data = $request->validate([
-            'director_id' => 'nullable',
-            'title' => 'required',
-            'rating' => 'nullable|decimal:0,2|between:0,10',
-            'has_seen' => 'nullable',
-            'description' => 'nullable'
-        ]);
+    public function update(Film $film, StoreFilmRequest $request) {
+        $data = $request->validated();
 
         $data['has_seen'] = $this->checkboxToBoolean($data);
 
